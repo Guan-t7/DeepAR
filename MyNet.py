Parameterize; fix train samples; apply scaling; Add preprocessing & visual

MyNet.py
@@ -16,8 +16,10 @@ DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 # parameters
 BATCH_SIZE = 64
-# N_EPOCHS = 20
-N_EPOCHS = 8
+N_EPOCHS = 12
+
+COND_STEPS = 7*24*4
+PRED_STEPS = 24*4
 
 # data
 # one sample: a window that forecast [7d:8d), conditioned by [0:7d)
@@ -25,17 +27,26 @@ class SeriesDataset(Dataset):
     def __init__(self, z, x, train: bool = True):
         self.z = z
         self.x = x
+        self.wnd = COND_STEPS+PRED_STEPS
+        # 1D separation between samples
+        self.stride = PRED_STEPS
 
     def __len__(self):
-        return self.z.shape[0] - 8*24*4 - 1
+        # actual wnd sz go beyond for 1 dp due to lstm char
+        return (self.z.shape[0] - self.wnd - 1) // self.stride + 1
 
     def __getitem__(self, index):
-        # todo 1D sep betw samples
-        z = self.z[index:index + 8*24*4]
-        x = self.x[index + 1:index + 1 + 8*24*4]
-        label = self.z[index + 1:index + 8*24*4 + 1]
+        index *= self.stride
+        # past observations; scale handling
+        z = self.z[...,index:index + self.wnd]
+        v = z[:COND_STEPS].mean(keepdims=True) + 1
+        # covariates
+        x = self.x[index + 1:index + 1 + self.wnd]
         data = np.concatenate((np.expand_dims(z, 1), x), axis=1)
-        return data, label
+        # forecast
+        index += COND_STEPS
+        label = self.z[index + 1:index + 1 + PRED_STEPS]
+        return data, v, label
 
 def get_data(bs):
     train_z = np.load(f"data/train_z.npy").astype('float32')
@@ -53,12 +64,14 @@ def get_data(bs):
 class MyNet(nn.Module):
     def __init__(self, params=None):
         super(MyNet, self).__init__()
-        self.lstm = nn.LSTM(input_size=1+5, hidden_size=40, num_layers=3, batch_first=True)
+        #todo multi..embed
+        self.lstm = nn.LSTM(input_size=1+5, hidden_size=40, num_layers=3,
+                            batch_first=True)
         self.distribution_mu = nn.Linear(40, 1)
         self.distribution_sigma = nn.Sequential(
             nn.Linear(40, 1),
             nn.Softplus(),
-        ) 
+        )
 
     def forward(self, x: 'torch.Tensor[float, float, float]', hx=None):
         lstm_out, hx = self.lstm(x, hx)
@@ -78,36 +91,32 @@ def get_model():
 
     return model, optimizer, loss_fn
 
-# feed nn a mini-batch, returning the loss
-def loss_batch(model, loss_fn, data, label, opt=None):
-    hx = loss_batch.hx
-    # Forward pass
-    mu, sigma, hx = model(data, hx)
-    loss_batch.hx = hx
-    loss = loss_fn(mu, sigma, label)
-
-    # Backward pass
-    if opt:
-        loss.backward()
-        opt.step()
-        opt.zero_grad()
-
-    return loss.item()
-
 # training loop
 def fit(epochs, model, loss_fn, opt, train_dl, valid_dl):
     Nb_batch = 0
     loss_train = .0
     for epoch in range(epochs):
         model.train()
-        for data, label in train_dl:
-            loss_batch.hx = None
-            # |condition|forecast|; treated the same in train
-            data, label = data.to(DEVICE), label.to(DEVICE)
-            loss_train += loss_batch(model, loss_fn, data, label, opt)
+        for data, v, label in train_dl:
+            data, v, label = data.to(DEVICE), v.to(DEVICE), label.to(DEVICE)
+            # scale input z
+            data[...,0] /= v
+            # feed nn a mini-batch. Forward pass
+            mu, sigma, hx = model(data)
+            # scale output
+            mu = mu[:, -PRED_STEPS:] * v
+            sigma = sigma[:, -PRED_STEPS:] * v
+            loss = loss_fn(mu, sigma, label)
+            # Backward pass
+            if opt:
+                loss.backward()
+                opt.step()
+                opt.zero_grad()
+            # accumulate loss
+            loss_train += loss.item()
             # log every ... mini-batches
-            if Nb_batch % 32 == 0:
-                writer.add_scalar('Loss/train', loss_train / (32*BATCH_SIZE),
+            if Nb_batch % 4 == 0:
+                writer.add_scalar('Loss/train', loss_train / (4*BATCH_SIZE),
                                 Nb_batch)
                 loss_train = .0
             Nb_batch += 1